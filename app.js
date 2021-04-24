c90ad686d745a3e19aeb5bca9f0a3c86858c584c
Jair Velazquez <velazquezespinozaeliasjair@gmail.com>
2021-04-24 13:41:07 -0500

    routing resourcers

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: false }));
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/resources/views/index.html'));
     //__dirname : It will resolve to your project folder.
-  });
+});
+
+router.get('/img/AurelionSol_0.jpg',function(req,res){
+    res.sendFile(path.join(__dirname+'/img/AurelionSol_0.jpg'));
+    //__dirname : It will resolve to your project folder.
+});
 
 app.use('/', router)
 // starting the server
