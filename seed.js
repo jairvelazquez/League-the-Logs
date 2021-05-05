a83cd52d113626d533108a9ba85b18d3137c6188
roylando <65689744+roylando@users.noreply.github.com>
2021-05-05 18:30:26 -0500

    cambios realizados funcional

--- seed.js
+++ seed.js
@@ -1,27 +1,27 @@
 var faker = require("faker");
 var Champions = require("./models/ChampionsModel");
 var mongoose = require("mongoose");
-const uri = "mongodb+srv://root_jair:9vB2T0XzD2Z1tfyk@cluster0.a98v9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority2";
+const uri = "mongodb+srv://root_roy:cowzEwl0lBU7dphZ@cluster0.iicdo.mongodb.net/test";
 console.log("Entra aquí");
 if (uri) {
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
   console.log("Entra aquí");
   var count = 0;
-  var num_records = 100;
+  var num_records = 3;
 
   for (var i = 0; i < num_records; i++) {
-    Champions.create(
+    const champ = new Champions(
       {
         id_champion: faker.datatype.number(),
         champion_name: faker.datatype.number(),
       },
-      function () {
-        count++;
-        if (count >= num_records) {
-          mongoose.connection.close();
-          console.log("Database Seeded");
-        }
-      }
     );
+    champ.save();
+    console.log("Id campeon: "+ champ.id_champion);
+    count++;
+    if (count >= num_records) {
+      //mongoose.connection.close();
+      console.log("Database Seeded");
+    }
   }
 }
