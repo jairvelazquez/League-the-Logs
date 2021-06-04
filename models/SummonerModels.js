fd8c0719ef0b6002d85561a337e677d4cb51f3bd
roylando <rodrigo.rodriguez7141@alumnos.udg.mx>
2021-06-04 18:59:03 -0500

    fix1

--- models/SummonerModels.js
+++ models/SummonerModels.js
@@ -4,10 +4,10 @@ const SummonerSchema = mongoose.Schema({
 	
 	account:{type: String, required: true},
 	summoner: {type: String, required: true}, 
-	nombre:{type: String, require: true},
+	name:{type: String, require: true},
 	level:{type: Number, require: true},
-	primaryRol:{type: String, require: true},
-	secondaryRol:{type: String, require: true}
+	primaryRole:{type: String, require: true},
+	secondaryRole:{type: String, require: true}
 });
 
 module.exports = mongoose.model('SummonerModel', SummonerSchema);
