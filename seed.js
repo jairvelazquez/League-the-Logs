fb0f04fe5c383defe6e3fe601767fe09f4762f05
jairvelazquez <43047639+jairvelazquez@users.noreply.github.com>
2021-06-16 14:44:57 -0500

    Deleting unused console logs

--- seed.js
+++ seed.js
@@ -2,10 +2,8 @@ var faker = require("faker");
 var Champions = require("./models/ChampionsModel");
 var mongoose = require("mongoose");
 const uri = "mongodb+srv://root_roy:cowzEwl0lBU7dphZ@cluster0.iicdo.mongodb.net/test";
-console.log("Entra aquí");
 if (uri) {
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
-  console.log("Entra aquí");
   var count = 0;
   var num_records = 20;
 
