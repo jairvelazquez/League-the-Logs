20f39a2a0f04c2d5686a4415782fd08437492128
roylando <rodrigo.rodriguez7141@alumnos.udg.mx>
2021-06-29 00:06:06 -0500

    fix2

--- models/UserModels.js
+++ models/UserModels.js
@@ -5,7 +5,7 @@ const UserSchema = mongoose.Schema({
 	username: {type: String,  required: true},
 	email: {type: String, required: true},
 	password: {type: String, required: true},
-	admin: {type: Boolean, value: false}
+	admin: {type: Boolean, default: false}
 
 });
 
